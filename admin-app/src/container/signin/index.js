d377040420d82a1eeb06c4010d4581074f917e25
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-02-23 19:00:09 +0530

    Sign Up Layout
    --Layout added

--- admin-app/src/container/signin/index.js
+++ admin-app/src/container/signin/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import Layout from '../../components/layout';
 
 /**
  * @author Ashish
  * @function Signin
  */
 
- const Signin = (props) =>{
-     return(
-         <div>Signin</div>
-     );
-} 
+const Signin = (props) => {
+    return (
+        <Layout>
+            <Container>
+                <Row style={{margin: '50px'}}>
+                    <Col md= {{ span: 6, offset:3 }}>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Sign In
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        </Layout>
+    );
+}
 
 export default Signin;
--- admin-app/src/container/signup/index.js
+++ admin-app/src/container/signup/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import Layout from '../../components/layout';
+import Input from '../../components/UI/Input';
 
 /**
  * @author Ashish
@@ -7,7 +10,51 @@ import React from 'react';
 
  const Signup = (props) =>{
      return(
-         <div>Signup</div>
+        <Layout>
+        <Container>
+            <Row style={{margin: '50px'}}>
+                <Col md= {{ span: 6, offset:3 }}>
+                    <Form>
+                        <Row>
+                            <Col md={6}>
+                                <Input
+                                    label="First Name"
+                                    placeholder="First Name"
+                                    value=""
+                                    type="text"
+                                    onChange={()=>{}}
+                                />
+                            </Col>
+                            <Col md={6}>
+                                <Input
+                                    label="Last Name"
+                                    placeholder="Last Name"
+                                    value=""
+                                    type="text"
+                                    onChange={()=>{}}
+                                />
+                            </Col>
+                        </Row>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Sign In
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    </Layout>
      );
 } 
 
